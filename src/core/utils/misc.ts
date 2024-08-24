fb5394ee3bc2de5a85c810f99b5734e86983620c
Predrag Kovacevic <predrag.kovacevicc@gmail.com>
2024-08-24 23:25:19 +0200

    reworking error handeling

--- src/core/utils/misc.ts
+++ src/core/utils/misc.ts
@@ -1,4 +1,3 @@
-import { Response } from 'express';
 import { ZodError } from 'zod';
 import { fromZodError } from 'zod-validation-error';
 import {
@@ -9,8 +8,10 @@ import {
   PrismaClientValidationError,
 } from '@prisma/client/runtime/library';
 
+import type { Response } from 'express';
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const checkIsPrismaError = (err: any) =>
+const checkIsPrismaError = (err: any) =>
   err instanceof PrismaClientKnownRequestError ||
   err instanceof PrismaClientUnknownRequestError ||
   err instanceof PrismaClientInitializationError ||
@@ -18,20 +19,18 @@ export const checkIsPrismaError = (err: any) =>
   err instanceof PrismaClientValidationError;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const checkIsZodError = (err: any) => err instanceof ZodError;
+const checkIsZodError = (err: any) => err instanceof ZodError;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getErrorStatusCode = (err: any, res: Response): number => {
-  const isPrismaError = checkIsPrismaError(err);
-  if (isPrismaError) return 404;
+  if (checkIsPrismaError(err)) return 404;
   if (checkIsZodError(err)) return 400;
   return err.statusCode ?? res.statusCode ?? 500;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getErrorMessage = (err: any): string => {
-  const isPrismaError = checkIsPrismaError(err);
-  if (isPrismaError) return 'Database Error';
+  if (checkIsPrismaError(err)) return 'Database Error';
   if (checkIsZodError(err)) return fromZodError(err).message;
   return err.message || 'Internal server error';
 };
